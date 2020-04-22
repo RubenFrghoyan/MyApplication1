2e8999775d65e2919e7f9f929cf0d4e64620b9a8
RubenFrghoyan <frghoyanruben@gmail.com>
2020-04-22 18:37:43 +0200

    code refactoring

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -10,16 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button start;
     private TextView textView;
     private EditText editText;
-    private int min;
     private int seconds;
-    private boolean isAlive = false;
 
-    private int sec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,88 +25,80 @@ public class MainActivity extends AppCompatActivity {
         start = findViewById(R.id.start);
         textView = findViewById(R.id.text_view);
         editText = findViewById(R.id.edit_text);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String s;
-                s = editText.getText().toString();
-                textView.setText(s);
-                try {
-                    min = Integer.parseInt(s.substring(0, s.indexOf(':')));
-                    sec = Integer.parseInt(s.substring(s.indexOf(':') + 1));
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this,"Please Insert a valid time: mm/ss",Toast.LENGTH_SHORT).show();
-                }
-                isAlive = true;
-                countdownTimer();
-            }
+        
+        start.setOnClickListener(this);
+        
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start:{
+                start();
+            }
+        }
+        
+    }
+
+    private void start() {
+        String s=editText.getText().toString();
+
+        try {
+            int min = Integer.parseInt(s.substring(0, s.indexOf(':')));
+            int sec = Integer.parseInt(s.substring(s.indexOf(':') + 1));
+            seconds = min*60+sec;
+
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this,"Please Insert a valid time: mm/ss",Toast.LENGTH_SHORT).show();
+        }
+        updateTextView(seconds);
+        countdownTimer();
+    }
+    private void updateTextView(int seconds){
+
+        if (seconds/60>=10&&seconds%60>=10) {
+            setTextView(seconds / 60 +":"+ seconds % 60);
+        }else
+        if (seconds/60<10&&seconds%60<10){
+            setTextView("0"+seconds / 60 +":"+ "0"+seconds % 60);
+        }else if (seconds/60<10&&seconds%60>=10) {
+            setTextView("0" + seconds / 60 + ":" + seconds % 60);
+        }else if (seconds/60>=10&&seconds%60<10){
+            setTextView(seconds / 60 +":"+ "0"+seconds % 60);
+        }
+
+    }
+
+    private void setTextView(final String time){
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                textView.setText(time);
+                if(seconds==0){
+                    textView.setTextColor(Color.RED);
+                }
+
+            }
         });
     }
 
-    class Timer extends Thread{
-        public void run(){
-            seconds = 60 * min + sec;
-
-            while (seconds != 0) {
-
-                seconds--;
-                if (seconds/60>=10&&seconds%60>=10) {
-                    setTextView(seconds / 60 +":"+ seconds % 60);
-                }else
-                    if (seconds/60<10&&seconds%60<10){
-                        setTextView("0"+seconds / 60 +":"+ "0"+seconds % 60);
-                    }else if (seconds/60<10&&seconds%60>=10) {
-                        setTextView("0" + seconds / 60 + ":" + seconds % 60);
-                    }else if (seconds/60>=10&&seconds%60<10){
-                        setTextView(seconds / 60 +":"+ "0"+seconds % 60);
-                    }
-
-
-                try {
-                    sleep(1000);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            isAlive=false;
-        }
-        }
-
-    /*private void setTextColor() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                textView.setTextColor(Color.RED);
-            }
-        });
-    }/*/
-
-    private void setTextView(final String time){
-            runOnUiThread(new Runnable() {
-
-                @Override
-                public void run() {
-
-                    textView.setText(time);
-
-                }
-            });
-        }
-
-
     private void countdownTimer(){
 
-        Timer t=new Timer();
+        Thread t=new Thread(){
+            public void run(){
+                while (seconds != 0) {
+                    seconds--;
+                    updateTextView(seconds);
+                    try {
+                        sleep(1000);
 
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
         t.start();
-        if (!isAlive){
-            textView.setTextColor(Color.RED);
-        }
-
-
-
-
     }
 }
