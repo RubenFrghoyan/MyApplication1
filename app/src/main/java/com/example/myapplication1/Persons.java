c933fee2c3b1f2f832f4791ddfb9180e791935fe
RubenFrghoyan <frghoyanruben@gmail.com>
2020-04-25 20:40:35 +0200

    added recycler view

---
+++ app/src/main/java/com/example/myapplication1/Persons.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    String name, surname;
+    int iD;
+
+    //constructor
+    public Person(String nm, String s, int id){
+        name=nm;
+        surname=s;
+        iD=id;
+    }
+}
